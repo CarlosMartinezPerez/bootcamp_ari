6ccdec962cd4143bf83a7cd06fd9c1d4982a2311
Carlos Martinez Perez <carlos.perez@compasso.com.br>
2023-07-16 21:10:25 -0300

    Atualizando projeto1 160723

--- projet1/main.py
+++ projet1/main.py
@@ -1,1 +1,8 @@
 import requests
+
+def deck_default(deck_count):
+    deck = requests.get(f'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count={deck_count}').json()
+    return deck
+
+qty_baralhos = 2  # Número de baralhos a embaralhar
+print(deck_default(qty_baralhos))
