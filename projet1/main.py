08820a8f85ba18e4414107243119e83b10457c2d
Carlos Martinez Perez <carlos.perez@compasso.com.br>
2023-07-18 04:05:52 -0300

    Atualizando CursoFlask 180723

--- projet1/main.py
+++ projet1/main.py
@@ -1,7 +1,7 @@
-import requests
+import requests as rq
 
 def deck_default(deck_count=1):
-    deck = requests.get(f'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count={deck_count}').json()
+    deck = rq.get(f'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count={deck_count}').json()
     return deck
 
 print(deck_default(4))  # Especificando o número de baralhos entre os parênteses
